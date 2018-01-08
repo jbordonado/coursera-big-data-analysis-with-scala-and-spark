58b6121598508defbf4731dfeceae1addbbf366d
Aigloun <jordan.bordonado@gmail.com>
2018-01-08 22:40:20 +0100

    Implementations of timeusage assignment

--- timeusage/src/main/scala/timeusage/TimeUsage.scala
+++ timeusage/src/main/scala/timeusage/TimeUsage.scala
@@ -62,15 +62,17 @@ object TimeUsage {
     *         have type Double. None of the fields are nullable.
     * @param columnNames Column names of the DataFrame
     */
-  def dfSchema(columnNames: List[String]): StructType =
-    ???
+  def dfSchema(columnNames: List[String]): StructType = {
+    StructType(
+      StructField(columnNames.head, StringType, nullable = false) :: columnNames.tail.map(name => StructField(name, DoubleType, nullable = false))
+    )
+  }
 
 
   /** @return An RDD Row compatible with the schema produced by `dfSchema`
     * @param line Raw fields
     */
-  def row(line: List[String]): Row =
-    ???
+  def row(line: List[String]): Row = Row.fromSeq((line.head :: line.tail.map(_.toDouble)))
 
   /** @return The initial data frame columns partitioned in three groups: primary needs (sleeping, eating, etc.),
     *         work and other (leisure activities)
@@ -88,7 +90,46 @@ object TimeUsage {
     *    “t10”, “t12”, “t13”, “t14”, “t15”, “t16” and “t18” (those which are not part of the previous groups only).
     */
   def classifiedColumns(columnNames: List[String]): (List[Column], List[Column], List[Column]) = {
-    ???
+    def isPrimaryNeeds(col: String): Boolean = {
+      col.startsWith("t01") ||
+      col.startsWith("t03") ||
+      col.startsWith("t11") ||
+      col.startsWith("t1801") ||
+      col.startsWith("t1803")
+    }
+    def isWork(col: String): Boolean = {
+      col.startsWith("t05") ||
+      col.startsWith("t1805")
+    }
+    def isOther(col: String): Boolean = {
+      col.startsWith("t02")||
+      col.startsWith("t04") ||
+      col.startsWith("t06") ||
+      col.startsWith("t07") ||
+      col.startsWith("t08") ||
+      col.startsWith("t09") ||
+      col.startsWith("t10") ||
+      col.startsWith("t12") ||
+      col.startsWith("t13") ||
+      col.startsWith("t14") ||
+      col.startsWith("t15") ||
+      col.startsWith("t16") ||
+      col.startsWith("t18")
+    }
+
+    val columnAcc = (List.empty[Column], List.empty[Column], List.empty[Column])
+
+    val classifiedColumns = columnNames.foldLeft(columnAcc) {
+      (acc, name) => {
+        val column = col(name)
+        if (isPrimaryNeeds(name)) (column :: acc._1, acc._2, acc._3)
+        else if (isWork(name)) (acc._1, column :: acc._2, acc._3)
+        else if (isOther(name)) (acc._1, acc._2, column :: acc._3)
+        else acc
+      }
+    }
+
+    classifiedColumns
   }
 
   /** @return a projection of the initial DataFrame such that all columns containing hours spent on primary needs
@@ -131,17 +172,18 @@ object TimeUsage {
     // more sense for our use case
     // Hint: you can use the `when` and `otherwise` Spark functions
     // Hint: don’t forget to give your columns the expected name with the `as` method
-    val workingStatusProjection: Column = ???
-    val sexProjection: Column = ???
-    val ageProjection: Column = ???
+    val workingStatusProjection: Column = when($"telfs".between(1, 3), "working").otherwise("not working").as("working")
+    val sexProjection: Column = when($"tesex" === 1, "male").otherwise("female").as("sex")
+    val ageProjection: Column = when($"teage".between(15, 23), "young").when($"teage".between(23, 56), "active").otherwise("elder").as("age")
 
     // Create columns that sum columns of the initial dataset
     // Hint: you want to create a complex column expression that sums other columns
     //       by using the `+` operator between them
     // Hint: don’t forget to convert the value to hours
-    val primaryNeedsProjection: Column = ???
-    val workProjection: Column = ???
-    val otherProjection: Column = ???
+    val primaryNeedsProjection: Column = primaryNeedsColumns.reduce(_ + _).divide(60).as("primaryNeeds")
+    val workProjection: Column = workColumns.reduce(_ + _).divide(60).as("work")
+    val otherProjection: Column = otherColumns.reduce(_ + _).divide(60).as("other")
+
     df
       .select(workingStatusProjection, sexProjection, ageProjection, primaryNeedsProjection, workProjection, otherProjection)
       .where($"telfs" <= 4) // Discard people who are not in labor force
@@ -165,7 +207,14 @@ object TimeUsage {
     * Finally, the resulting DataFrame should be sorted by working status, sex and age.
     */
   def timeUsageGrouped(summed: DataFrame): DataFrame = {
-    ???
+    summed.select($"working", $"sex", $"age", $"primaryNeeds", $"work", $"other")
+          .groupBy($"working", $"sex", $"age")
+          .agg(
+            round(avg("primaryNeeds"), 1).as("primaryNeeds"),
+            round(avg("work"), 1).as("work"),
+            round(avg("other"), 1).as("other")
+          )
+          .orderBy($"working", $"sex", $"age")
   }
 
   /**
@@ -182,7 +231,10 @@ object TimeUsage {
     * @param viewName Name of the SQL view to use
     */
   def timeUsageGroupedSqlQuery(viewName: String): String =
-    ???
+    "SELECT working, sex, age, ROUND(AVG(primaryNeeds), 1) AS primaryNeeds, ROUND(AVG(work), 1) AS work, ROUND(AVG(other), 1) AS other " +
+      "FROM " + viewName + " " +
+      "GROUP BY working, sex, age " +
+      "ORDER BY working, sex, age"
 
   /**
     * @return A `Dataset[TimeUsageRow]` from the “untyped” `DataFrame`
@@ -192,7 +244,7 @@ object TimeUsage {
     * cast them at the same time.
     */
   def timeUsageSummaryTyped(timeUsageSummaryDf: DataFrame): Dataset[TimeUsageRow] =
-    ???
+    timeUsageSummaryDf.map(row => TimeUsageRow(row.getAs("working"), row.getAs("sex"), row.getAs("age"), row.getAs("primaryNeeds"), row.getAs("work"), row.getAs("other")))
 
   /**
     * @return Same as `timeUsageGrouped`, but using the typed API when possible
@@ -207,7 +259,15 @@ object TimeUsage {
     */
   def timeUsageGroupedTyped(summed: Dataset[TimeUsageRow]): Dataset[TimeUsageRow] = {
     import org.apache.spark.sql.expressions.scalalang.typed
-    ???
+
+    summed.groupByKey(r => (r.working, r.sex, r.age))
+          .agg(
+            round(typed.avg[TimeUsageRow](_.primaryNeeds), 1).as(Encoders.DOUBLE),
+            round(typed.avg[TimeUsageRow](_.work), 1).as(Encoders.DOUBLE),
+            round(typed.avg[TimeUsageRow](_.other), 1).as(Encoders.DOUBLE)
+          )
+        .map{ case ((working, sex, age), primaryNeeds, work, other) => TimeUsageRow(working, sex, age,  primaryNeeds, work, other) }
+        .orderBy('working, 'sex, 'age)
   }
 }
 
--- timeusage/src/test/scala/timeusage/TimeUsageSuite.scala
+++ timeusage/src/test/scala/timeusage/TimeUsageSuite.scala
@@ -1,19 +1,54 @@
 package timeusage
 
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.{ColumnName, DataFrame, Row}
-import org.apache.spark.sql.types.{
-  DoubleType,
-  StringType,
-  StructField,
-  StructType
-}
+import org.apache.spark.sql.types.{DoubleType, StringType, StructField, StructType}
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.{BeforeAndAfterAll, FunSuite}
 
-import scala.util.Random
-
 @RunWith(classOf[JUnitRunner])
 class TimeUsageSuite extends FunSuite with BeforeAndAfterAll {
+  val columnsName: List[String] = List(
+    "t010101",
+    "t010102",
+    "t010199",
+    "t010201",
+    "t010299",
+    "t020699",
+    "t020701",
+    "t020799",
+    "t030101",
+    "t030102",
+    "t030103",
+    "t030104",
+    "t030105",
+    "t030108",
+    "t030109",
+    "t050189",
+    "t050201",
+    "t050202",
+    "t050203",
+    "t180104",
+    "t180305",
+    "t180599",
+    "t180502",
+    "t181181"
+  )
 
+  test("classifiedColumns") {
+    val (primaryNeeds, work, other) = TimeUsage.classifiedColumns(columnsName)
+
+    println("-------------- Primary Needs --------------")
+    primaryNeeds.foreach(println)
+    println("-------------- Work --------------")
+    work.foreach(println)
+    println("-------------- Other --------------")
+    other.foreach(println)
+
+    assert(primaryNeeds.size === 5 + 7 + 0 + 1 + 1) // “t01”, “t03”, “t11”, “t1801” and “t1803”
+    assert(work.size === 4 + 2) // “t05” and “t1805”
+    assert(other.size === 3 + 0 + 0 + 0 + 0 + 0 + 0 + 0 + 0 + 0 + 0 + 0 + 1) // “t02”, “t04”, “t06”, “t07”, “t08”, “t09”, “t10”, “t12”, “t13”, “t14”, “t15”, “t16” and “t18” (those which are not part of the previous groups only).
+  }
 }
